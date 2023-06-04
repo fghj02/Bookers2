c9e54692d6d1de8cbaeac0e6bcbf939b74bd318b
EC2 Default User <ec2-user@ip-172-31-27-100.ap-northeast-1.compute.internal>
2023-06-04 00:11:32 +0000

    toukou login

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,6 +15,12 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
     @book = Book.new
+
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to books_path
+
   end
 
   def show
